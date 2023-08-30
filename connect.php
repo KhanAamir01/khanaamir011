7494b7743c5ab9fc0bf29e0bf887abdcabf15ae3
KhanAamir01 <143620276+KhanAamir01@users.noreply.github.com>
2023-08-30 19:05:44 +0530

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,9 @@
+<? php
+    $fname = $_POST['fname'];
+	$lname = $_POST['lname'];
+	$age = $_POST['age'];
+	$address = $_POST['address'];
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	$gender = $_POST['gender'];
+?>
